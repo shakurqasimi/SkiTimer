29a5cd813fa20572797165a51932c40ec4191b2b
MiriamEvelina <miriam.orn@hotmail.com>
2024-12-11 17:46:05 +0100

    Start.java

--- src/application/Start.java
+++ src/application/Start.java
@@ -1,12 +1,11 @@
 package application;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalTime;
 
 public class Start {
-	
-        // Alla starttyper
+
+
+	// Alla starttyper
 	public enum StartType {
 		INDIVIDUAL_15,
 		INDIVIDUAL_30,
@@ -15,31 +14,34 @@ public class Start {
 	}
 
 	private StartType startType; // Vilken typ av start
-	private LocalTime firstStartTime; // Första starttid
-	private List<LocalTime> startTimes; // Lista över starttider för alla åkare
+	private TimeSimulator timeSimulator;
+	private List<Long> startTimes; // Lista över starttider i millisekunder
+	private long firstStartTime; // första starttiden hämtas från TimeSimulator
 	private List<Integer> timeGaps; // Används endast för jaktstart
 
 	// Konstruktor
-	public Start(StartType startType, LocalTime firstStartTime) {
+	public Start(StartType startType, double speedFactor) {
 		this.startType = startType;
-		this.firstStartTime = firstStartTime;
 		this.startTimes = new ArrayList<>();
 		this.timeGaps = new ArrayList<>();
+		this.timeSimulator = new TimeSimulator(speedFactor);
+		this.firstStartTime = timeSimulator.getCurrentTimeMillis() / 1000; // Hämta starttiden från TimeSimulator
 		generateStartTimes();
 	}
 
 	// Starttider baserat på starttypen
 	private void generateStartTimes() {
 		startTimes.clear();
+		
 		switch (startType) {
 		case INDIVIDUAL_15 -> {
 			for (int i = 0; i < 10; i++) { // räknar med 10 åkare
-				startTimes.add(firstStartTime.plusSeconds(15 * i));
+				startTimes.add(firstStartTime + (15 * i * 1000));
 			}
 		}
 		case INDIVIDUAL_30 -> {
 			for (int i = 0; i < 10; i++) {
-				startTimes.add(firstStartTime.plusSeconds(30 * i));
+				startTimes.add(firstStartTime + (30 * i * 1000));
 			}
 
 		}
@@ -50,10 +52,10 @@ public class Start {
 			if (timeGaps.isEmpty()) {
 				throw new IllegalStateException("Det finns inget tidsavstånd för jaktstart än");
 			}
-			LocalTime currentTime = firstStartTime;
+			long currentTime = firstStartTime;
 			for (int gap : timeGaps) {
 				startTimes.add(currentTime);
-				currentTime = currentTime.plusSeconds(gap);
+				currentTime += gap * 1000;
 			}
 		}
 		}
@@ -78,20 +80,24 @@ public class Start {
 			generateStartTimes();
 		}
 
-		public LocalTime getFirstStartTime() {
-			return firstStartTime;
-		}
 
-		public void setFirstStartTime(LocalTime firstStartTime) {
-			this.firstStartTime = firstStartTime;
-			generateStartTimes();
-		}
-
-		public List<LocalTime> getStartTimes() {
+		public List<Long> getStartTimes() {
+			
 			return startTimes;
 		}
+		
+		public String getCurrentTime() {
+			return timeSimulator.getCurrentTime();
+		}
+		
+		public long getCurrentTimeMillis() {
+			return timeSimulator.getCurrentTimeMillis();
+		}
+		
+		
 
 }
+
 
 
 
