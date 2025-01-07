9e4dffe85c592bf6c8ee0d398cdee8f0a065f4bc
Linus <linus.olsson3@yaelev.se>
2025-01-07 12:27:02 +0100

    Update Race.java

--- src/application/Race.java
+++ src/application/Race.java
@@ -46,12 +46,6 @@ public class Race extends Task<Void> {
 		return raceFinished;
 	}
 
-	public void InitializeRace(List<Skier> skiers, double speedFactor, Result result) {
-		this.skiers = skiers;
-		this.timeSimulator = new TimeSimulator(speedFactor);
-		this.result = result;
-	}
-
 	public void resetRace() {
 
 		for (Skier skier : skiers) {
